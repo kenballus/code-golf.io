5ec0102e87dcc25563d9ae24a29927c34f59d85c
Ben Kallus <49924171+kenballus@users.noreply.github.com>
2019-11-14 23:28:26 -0500

    Update Divisors.py

--- Divisors.py
+++ Divisors.py
@@ -1,5 +1,2 @@
-def d(n,m):
- if m<1:return[]
- if n%m<1:return d(n,m-1)+[str(m)]
- return d(n,m-1)
-for i in range(1,101):print(" ".join(d(i,i)))
+def d(n,m):return[]if m<1else d(n,m-1)+[str(m)]if n%m<1else d(n,m-1)
+for i in range(1,101):print(*d(i,i))
