b520a2c451ca05e3975d8f089875a30522f74541
Ben Kallus <49924171+kenballus@users.noreply.github.com>
2019-12-04 13:54:03 -0500

    Create Diamonds.rb

---
+++ Diamonds.rb
@@ -0,0 +1,16 @@
+(1..9).each{|m|
+(1..m).each{|n|
+print" "*(10-n)
+(1..n).each{|i|
+print i}
+(2..n).each{|i|
+print n-i+1}
+puts}
+(1..m).each{|n|
+n=m-n
+print" "*(10-n)
+(1..n).each{|i|
+print i}
+(2..n).each{|i|
+print n-i+1}
+puts}}
