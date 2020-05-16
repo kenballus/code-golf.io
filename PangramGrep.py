f1cb04d779ff2b643be6b50f79548e3de92063df
Ben Kallus <49924171+kenballus@users.noreply.github.com>
2020-05-16 00:17:58 -0400

    Added &=

--- PangramGrep.py
+++ PangramGrep.py
@@ -1,6 +1,5 @@
 import sys
 for s in sys.argv[1:]:
- fail=1
- for j in range(65,91):
-  if chr(j)not in s and chr(j).lower()not in s:f=0
+ f=1
+ for j in range(65,91):f&=chr(j)in s or chr(j).lower()in s
  if f:print(s)
