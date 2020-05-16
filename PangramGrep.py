753b0a7ee71bdda10434fb66e03a6ce609fe6009
Ben Kallus <49924171+kenballus@users.noreply.github.com>
2020-05-16 00:13:01 -0400

    Changed fail to f

--- PangramGrep.py
+++ PangramGrep.py
@@ -2,5 +2,5 @@ import sys
 for s in sys.argv[1:]:
  fail=1
  for j in range(65,91):
-  if chr(j)not in s and chr(j).lower()not in s:fail=0
- if fail:print(s)
+  if chr(j)not in s and chr(j).lower()not in s:f=0
+ if f:print(s)
