3b65051b0edb95f54d2a5ed73c066aa445a3f751
Ben Kallus <49924171+kenballus@users.noreply.github.com>
2021-07-30 13:58:44 +0000

    Update Divisors.py
    100 characters->87 characters.
    Replaced a def with a lambda and removed a parameter.

--- Divisors.py
+++ Divisors.py
@@ -1,2 +1,2 @@
-def d(n,m):return[]if m<1else d(n,m-1)+[m]if n%m<1else d(n,m-1)
-for i in range(1,101):print(*d(i,i))
+d=lambda m:(d(m-1)if i%m else d(m-1)+[m])if m else[]
+for i in range(1,101):print(*d(i))
