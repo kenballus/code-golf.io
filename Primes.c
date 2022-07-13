e60e328bda8478688c2daa96a84d2a0aabaa38d4
Ben Kallus <49924171+kenballus@users.noreply.github.com>
2022-07-13 03:17:40 +0000

    Update Primes.c

--- Primes.c
+++ Primes.c
@@ -1,1 +1,1 @@
-#include<stdio.h>int main(){for(int j=2;j<98;j++){int a=1;for(int i=2;i<j;i++){if(j%i<1)a=0;}if(a>0)printf("%i\n",j);}return 0;}
+#include<stdio.h>int i,j,a,main(){for(j=2;j<98;j++){a=1;for(i=2;i<j;i++){if(j%i<1)a=0;}if(a>0)printf("%i\n",j);}}
