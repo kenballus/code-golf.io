440c35ab68b4b5a6d825d0c0aa043ba5377fedea
Ben Kallus <49924171+kenballus@users.noreply.github.com>
2022-08-16 23:58:26 +0000

    Update Emirp.py

--- Emirp.py
+++ Emirp.py
@@ -1,7 +1,3 @@
-def p(n):
- for i in range(2,n):
-  if n%i<1:return 0
- return 1
-for i in range(999):
- r=int(str(i)[::-1])
- if p(i)and p(r)and i!=r:print(i)
+p=lambda n:all(n%i for i in range(2,n))
+for i in range(992):
+ if p(i)and p(r:=int(str(i)[::-1]))and i!=r:print(i)
