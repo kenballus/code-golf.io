6809b0cb58c018c7d27fac90c1b4f1f83dcc20b1
Ben Kallus <49924171+kenballus@users.noreply.github.com>
2022-08-17 00:16:05 +0000

    Update Diamonds.py

--- Diamonds.py
+++ Diamonds.py
@@ -1,3 +1,4 @@
-for m in range(2,11):
- for n in range(1,m):s="".join(map(str,range(1,n+1)));print(" "*(10-n)+s+s[::-1][1:])
- for n in range(1,m):s="".join(map(str,range(1,m-n)))[::-1];print(" "*(11-m+n)+s[::-1][:-1]+s)
+r=range
+for m in r(2,11):
+ for n in r(1,m):s="".join(map(str,r(1,n+1)));print(" "*(10-n)+s+s[::-1][1:])
+ for n in r(1,m):s="".join(map(str,r(1,m-n)))[::-1];print(" "*(11-m+n)+s[::-1][:-1]+s)
