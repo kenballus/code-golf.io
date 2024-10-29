b94135c2a916b346f99dc35e2c5344e331b2d6de
Ben Kallus <49924171+kenballus@users.noreply.github.com>
2024-10-29 00:26:27 +0000

    Update PangramGrep.py

--- PangramGrep.py
+++ PangramGrep.py
@@ -1,5 +1,3 @@
 import sys
 for s in sys.argv[1:]:
- f=1
- for j in range(65,91):f&=chr(j)in s or chr(j).lower()in s
- if f:print(s)
+ if all(chr(j)in s or chr(j).lower()in s for j in range(65,91)):print(s)
